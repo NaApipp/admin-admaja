f8f52f12ea555dca6ce924894ccd1ee39db4c7ad
NaApipp <nabilapipp@gmail.com>
2026-09-03 15:15:19 +0700

    feat: import and implementation components for general part

--- app/(dashboard)/general/page.tsx
+++ app/(dashboard)/general/page.tsx
@@ -1,6 +1,17 @@
+import Header from "./components/Header"
+import TotalMemberActive from "./components/TotalMemberActive"
+import TotalAdmin from "./components/TotalAdmin"
+import DataAdmin from "./components/DataAdmin"
+
 export default function GeneralPage() {
     return (
-        <div className="bg-main">
+        <div>
+            <Header />
+            <div className="flex flex-col md:flex-row gap-2">
+                <TotalMemberActive />
+                <TotalAdmin />
+            </div>
+            <DataAdmin />
         </div>
     )
 }
