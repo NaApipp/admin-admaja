e9717995ad4335747eb442d5ae8a07de329a3535
NaApipp <nabilapipp@gmail.com>
2026-09-03 11:12:05 +0700

    chore: create api for add super_admin role

---
+++ app/api/auth/3f0ba3cd87ca54b484ebe82e0c123deddddb37c2641d391c91487df6af1832e2/route.ts
@@ -0,0 +1,146 @@
+import { withCors, handleOptions } from "@/app/lib/cors";
+import { NextRequest, NextResponse } from "next/server";
+import clientPromise from "@/app/lib/mongodb";
+import bcrypt from "bcryptjs";
+import z from "zod";
+
+function formatDateWIB(date: Date) {
+  const fmt = new Intl.DateTimeFormat("en-GB", {
+    timeZone: "Asia/Jakarta",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
+
+  const parts = fmt.formatToParts(date);
+  const map: Record<string, string> = {};
+  for (const p of parts) map[p.type] = p.value;
+
+  return `${map.day}/${map.month}/${map.year} ${map.hour}:${map.minute}:${map.second}`;
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const registerSchema = z.object({
+      // Name
+      name: z
+        .string()
+        .trim()
+        .min(1, "Nama depan harus diisi")
+        .max(50, "Nama depan maksimal 50 karakter"),
+        // .regex(/^[A-Za-zÀ-ÿ\s]+$/, {
+        //   message: "Nama depan hanya boleh mengandung huruf dan spasi",
+        // }),
+
+      // Angkatan
+      angkatan: z
+        .string()
+        .trim()
+        .min(1, "Angkatan harus diisi")
+        .regex(/^\d{4}$/, {
+          message: "Angkatan harus berupa 4 digit angka",
+        }),
+
+      // Validasi Level
+      password: z
+        .string()
+        .min(8, "Password minimal 8 karakter")
+        .max(100, "Password maksimal 100 karakter"),
+
+      // Role
+      role: z.enum(["admin", "super_admin"], {
+        message: "Role tidak valid",
+      }),
+
+      // Status
+      status: z.enum(["aktif", "nonaktif", "purna"], {
+        message: "Status tidak valid",
+      }),
+    });
+
+    const validation = registerSchema.safeParse(body);
+
+    if (!validation.success) {
+      return withCors(
+        NextResponse.json(
+          {
+            message: validation.error.issues[0].message,
+            errors: validation.error.issues,
+          },
+          { status: 400 },
+        ),
+        req,
+      );
+    }
+
+    const { name, angkatan, password, role, status } = validation.data;
+
+    const client = await clientPromise;
+    // DB and Colecction Name
+    const db = client.db(process.env.MONGODB_DATABASE);
+    const usersCollection = db.collection("user_admin");
+
+    //  Vakidation check on db name
+    const existingUser = await usersCollection.findOne({
+      $or: [{ name }],
+    });
+    if (existingUser) {
+      return withCors(
+        NextResponse.json({ message: "Nama sudah terdaftar" }, { status: 400 }),
+        req,
+      );
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Time Format create Account
+    // const formattedDate = formatDateWIB(new Date());
+    const now = new Date();
+    const formattedDate = now.toDateString();
+
+    // 🆔 Generate ID
+    const uniqueId = Math.random().toString(36).substring(2, 8).toUpperCase();
+    const user_id = `USR-ADM-${uniqueId}`;
+
+    // Saving New User
+    const result = await usersCollection.insertOne({
+      user_id,
+      name: name,
+      angkatan: angkatan,
+      role: role,
+      status: status,
+      password: hashedPassword,
+      createdAt: formattedDate,
+    });
+
+    return withCors(
+      NextResponse.json(
+        {
+          success: true,
+          message: "Tambah Data User Admin Berhasil",
+          user_id: user_id,
+        },
+        { status: 201 },
+      ),
+      req,
+    );
+  } catch (error) {
+    console.error("Registration error:", error);
+    return withCors(
+      NextResponse.json(
+        { success: false, message: "Terjadi kesalahan server" },
+        { status: 500 },
+      ),
+      req,
+    );
+  }
+}
+
+export const OPTIONS = handleOptions;
